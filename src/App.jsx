3cf71dae2d84c6c7335a7405b883927aad271e1e
shivamsinghAIMLops32 <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2024-10-03 18:00:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <button onClick={()=>{setCount((count) => count+1)}}>increment</button>
+      <br />
+      <h3>count is {count}</h3>
+      <br />
+      <button onClick={()=>{setCount((count)=>count -1)}}>decrement</button>
+    </>
+  );
+}
+
+export default App;
